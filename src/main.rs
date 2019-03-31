72b14e069b2136ef8cede8259491d868ad5460a5
LiteUnder <alxjacobs14@gmail.com>
2019-03-31 13:26:47 -0600

    added a function and some notes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, world!");
+    println!("{0}", add(7, 9)) // last line of scope doesn't need a semi colon, interesting
+}
+
+// A few interesting notes on syntax here
+// paramaters are declared as <name>: <type>
+// return type isn't required, but syntax is -> <type>
+fn add(a: i32, b: i32) -> i32 {
+    a + b // last line of function is always run as a return
 }
