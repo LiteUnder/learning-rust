a062118d8913026c07672363ec091a438d9b5a81
LiteUnder <alxjacobs14@gmail.com>
2019-03-31 13:28:43 -0600

    more notes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 fn main() {
+    // when function has ! at the end, it's a macro
+    // idk the difference yet
     println!("Hello, world!");
+
+    // println! macro only takes string literal
+    // {0} is used for formatting
+    // "{0} {1}", 12, 13 will evalulate as "12 13"
+    // numebered values start from 0, nice
     println!("{0}", add(7, 9)) // last line of scope doesn't need a semi colon, interesting
 }
 
